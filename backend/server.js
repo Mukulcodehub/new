13aeb09eaaf5c5fa885cd863d0283996b57744a3
Mukul-joye <mukul-joye.ai>
2025-11-18 13:31:48 +0530

    Fix file path

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/connectDB');
+const connectDB = require('../backend/config/connectDB');
 
 // Load env vars
 dotenv.config();
